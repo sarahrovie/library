c1f3d0313e9562c2db92dd5184ce78a6355178ac
Sarah R. Vieira <sarahrdvieira@gmail.com>
2025-09-21 15:59:04 -0300

    Add button to remove book

--- main.js
+++ main.js
@@ -38,11 +38,17 @@ function displayBooks() {
     pages.textContent = `Pages: ${book.pages}`;
 
     const read = document.createElement('p');
-    read.textContent = `${book.read ? 'read' : 'not read'}`;
+    read.textContent = `${book.read ? 'Read' : 'Not read'}`;
     read.style.color = `${book.read ? 'green' : 'red'}`;
 
+    const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>window-close</title><path d="M13.46,12L19,17.54V19H17.54L12,13.46L6.46,19H5V17.54L10.54,12L5,6.46V5H6.46L12,10.54L17.54,5H19V6.46L13.46,12Z" /></svg>`;
+    const removeBtn = document.createElement('button');
+    removeBtn.classList = 'remove-btn';
+    removeBtn.setAttribute('data-book-id', `${book.id}`);
+    removeBtn.innerHTML += svg;
+
     info.append(author, pages, read);
-    card.append(title, info);
+    card.append(removeBtn, title, info);
     main.appendChild(card);
   }
 }
